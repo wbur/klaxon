bc3e5502df4ca8930118aaa54b4828dd5d7a0d09
William Smith <will@wfs.io>
2025-11-12 18:49:39 -0500

    Revert "attempting to fix 422 issue."
    This reverts commit 063921e7a40ddccc14c27aebbc02cca67f505145.

--- config/initializers/session_store.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.session_store :cookie_store,
-  key: '_klaxon_session',
-  same_site: :none,
-  secure: Rails.env.production?  # ensures cookie sent only over HTTPS in production
